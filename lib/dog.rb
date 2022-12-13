882f533b1c885d95df7ed940fde3362602443a25
tor <torfrancis447@gmail.com>
2022-12-13 16:29:36 -0500

    commit tree cece7917cc96eeabf5cfd058f7e8831e78473cbc

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
+require "pry"
 class Dog
+    attr_writer :name, :breed
+    attr_reader :name, :breed
+
+    def name=(name)
+        @name= name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed= breed
+    end
+
+    def breed
+        @breed
+    end    
 
 end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
+    attr_writer :name, :job
+    
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+    def job
+        @job
+    end
 
 end
+
+alena = Person.new
+
